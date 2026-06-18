475f891744f3e695a985d094d40d6ae95ec56c52
Cain Brouwer <caindavid2008@gmail.com>
2026-06-18 22:03:56 +0200

    fix: EffectComposer altijd gemount, Bloom conditoneel, geen FloatType

--- src/components/TimelineScene.jsx
+++ src/components/TimelineScene.jsx
@@ -752,11 +752,9 @@ function SceneContent() {
 
   return (
     <>
-      {bloomEnabled && (
-        <EffectComposer>
-          <Bloom luminanceThreshold={0.15} luminanceSmoothing={0.9} intensity={0.5} mipmapBlur />
-        </EffectComposer>
-      )}
+      <EffectComposer>
+        {bloomEnabled && <Bloom luminanceThreshold={0.15} luminanceSmoothing={0.9} intensity={0.5} mipmapBlur />}
+      </EffectComposer>
 
       <CameraMetrics />
       <fog attach="fog" args={['#0a0015', 80, 800]} />
